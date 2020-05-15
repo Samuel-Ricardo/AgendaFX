c1132fb742a50b9a2b5a61e14f49e769e856146a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-05-15 13:03:39 -0300

    Comentando: ChooserController

--- src/Controller/ChooserController.java
+++ src/Controller/ChooserController.java
@@ -32,6 +32,7 @@ public class ChooserController implements Initializable {
     private Pane ChooserPane;
     @FXML
     private ComboBox<String> cbType;
+    
     private ArrayList<String> alTypes = new ArrayList<>();
     private ObservableList<String> obsTypes;
     private int index;
@@ -39,29 +40,30 @@ public class ChooserController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) {        // execute when starting // executa ao iniciar
 
-        alTypes.add("Notficaçao");
-        alTypes.add("PostIt");
+        alTypes.add("Notficaçao");  // Load the Array List with the options // Carrega o ArrayList com as opçoes;
+        alTypes.add("PostIt");      
         alTypes.add("Atividade");
 
-        obsTypes = FXCollections.observableArrayList(alTypes);
+        obsTypes = FXCollections.observableArrayList(alTypes);  // Convert the ArrayList to ObservableList // Converte o ArrayList para ObservableList
 
-        cbType.setItems(obsTypes);
+        cbType.setItems(obsTypes);                              // Load the ComboBox with the ObservableList // Carrega o ComboBox com o ObservableList
 
     }
 
     @FXML
-    public void createThis(ActionEvent event) {
+    public void createThis(ActionEvent event) {                    // Opens the creation window corresponding to the one selected // Abre a janela de criaçao correspondente ao que foi selecionado
 
-        index = cbType.getSelectionModel().getSelectedIndex();
+        index = cbType.getSelectionModel().getSelectedIndex();   // get what was selected in the ComboBox  //  pega o que foi selecionado no ComboBox
 
-        switch (index) {
+        switch (index) {                                       // choose the window to open  //  escolhe a janela a ser aberta
 
             case 0:
 
-                if (MainNotificationCreator.getWindow() != null) {
+                if (MainNotificationCreator.getWindow() != null) {  // Close the window if it is open // Fecha a janela se estiver aberta
 
                     MainNotificationCreator.getWindow().close();
                 }
@@ -69,7 +71,7 @@ public class ChooserController implements Initializable {
                 MainNotificationCreator not = new MainNotificationCreator();
 
                 try {
-                    not.start(new Stage());
+                    not.start(new Stage());         // Open the window // abre a janela
                 } catch (Exception ex) {
                     Logger.getLogger(ChooserController.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -77,9 +79,11 @@ public class ChooserController implements Initializable {
                 break;
 
         }
-        MainChooser.getWindow().close();
+        MainChooser.getWindow().close();      // closes the current window  // fecha a janela atual
     }
 
+        //////Getters and Setters////////
+    
     public Pane getChooserPane() {
         return ChooserPane;
     }
