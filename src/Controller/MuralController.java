fcce4d441e9f7f97f6a622d0c67524c7433fb813
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-06-08 10:26:05 -0300

    Mural has been created

---
+++ src/Controller/MuralController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.TilePane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Samuel
+ */
+public class MuralController implements Initializable {
+    
+    @FXML
+    private TilePane tlMural;
+
+
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
